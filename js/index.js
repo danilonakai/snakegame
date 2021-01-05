6d5043ff6fcc4c08eb0b2495f808e8e48e79027b
Danilo Nakai <contact.danilonakai@gmail.com>
2021-01-05 11:04:47 +0900

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,90 @@
+window.onload = function() {
+    var stage = document.querySelector('#stage');
+    var context = stage.getContext("2d");
+
+    const velocity = 1; // quantas casas anda a cada vez que o jogo stage atualizar
+    var moveX = moveY = 0; //Velocidade para cada lado
+    var snakeX = snakeY = 10; //Ponto de inicio
+    var pieceSize = 20; //Tamanho de cada peça
+    var pieceQuantity = 20; //Quantidade de peças que tem no stage
+    var orangeX = orangeY = 18;
+
+    var trail = []; //Rastro da cobra
+    var tail = 5; //Rabo da cobra
+
+    document.addEventListener("keydown", keyPush);
+    setInterval(game, 60);
+
+    function game(){
+        snakeX += moveX;
+        snakeY += moveY;
+
+        if(snakeX < 0){
+            snakeX = pieceQuantity - 1;
+        }
+        if(snakeX > pieceQuantity - 1){
+            snakeX = 0;
+        }
+        if(snakeY < 0){
+            snakeY = pieceQuantity - 1;
+        }
+        if(snakeY > pieceQuantity - 1){
+            snakeY = 0;
+        }
+        
+        // Fill the stage
+        context.fillStyle = "black";
+        context.fillRect(0, 0, stage.width, stage.height);
+
+        // Fill orange
+        context.fillStyle = "orange";
+        context.fillRect(orangeX * pieceSize, orangeY * pieceSize, pieceSize, pieceSize);
+
+        // Fill snake
+        context.fillStyle = "gray";
+        for(var i = 0; i < trail.length; i++){
+            context.fillRect(trail[i].x * pieceSize, trail[i].y * pieceSize, pieceSize, pieceSize);
+            if(trail[i].x == snakeX && trail[i].y == snakeY){
+                moveX = moveY = 0;
+                tail = 5;
+            }
+        }
+        
+        trail.push({x:snakeX, y:snakeY});
+
+        while(trail.length > tail){
+            trail.shift();
+        }
+
+        if(orangeX == snakeX && orangeY == snakeY){
+            tail++;
+            orangeX = Math.floor(Math.random() * pieceQuantity);
+            orangeY = Math.floor(Math.random() * pieceQuantity);
+        }
+    }
+
+    function keyPush(event){
+        switch(event.keyCode){
+            case 37: //left
+                moveX = -velocity;
+                moveY = 0;
+            break;
+            
+            case 38: //up
+                moveX = 0;
+                moveY = -velocity;
+            break;
+
+            case 39: //right
+                moveX = velocity;
+                moveY = 0;
+            break;
+
+            case 40: //down
+                moveX = 0;
+                moveY = velocity;
+            break;
+        }
+    }
+    
+}
